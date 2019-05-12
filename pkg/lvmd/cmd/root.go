8060a7a1a0da07c68f0828da22485329f0974ace
ymmt <ymmt2005@gmail.com>
2019-05-13 05:26:33 +0900

    fix

--- pkg/lvmd/cmd/root.go
+++ pkg/lvmd/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
@@ -42,6 +43,9 @@ func subMain() error {
 		return err
 	}
 
+	// UNIX domain socket file should be removed before listening.
+	os.Remove(config.socketName)
+
 	lis, err := net.Listen("unix", config.socketName)
 	if err != nil {
 		return err
@@ -49,7 +53,19 @@ func subMain() error {
 	grpcServer := grpc.NewServer()
 	proto.RegisterLVServiceServer(grpcServer, lvmd.NewLVService(vg))
 	proto.RegisterVGServiceServer(grpcServer, lvmd.NewVGService(vg))
-	return grpcServer.Serve(lis)
+	well.Go(func(ctx context.Context) error {
+		return grpcServer.Serve(lis)
+	})
+	well.Go(func(ctx context.Context) error {
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+		return nil
+	})
+	err = well.Wait()
+	if err != nil && !well.IsSignaled(err) {
+		return err
+	}
+	return nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
