0136dc08189d5c96c4f4f9638437f8bb6ef3ed2b
Kazuhito MATSUDA <kazuto.jinnai@gmail.com>
2019-05-17 14:00:16 +0900

    [scheduler] writing ginkgo tests on microk8s...

--- microtest/scheduler_test.go
+++ microtest/scheduler_test.go
@@ -2,17 +2,64 @@ package microtest_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 )
 
 var _ = Describe("Test topolvm-scheduler", func() {
 	It("should be deployed topolvm-scheduler pod", func() {
+		Eventually(func() error {
+			result, err := kubectl("get", "-n=kube-system", "pods", "--selector=app.kubernetes.io/name=topolvm-scheduler", "-o=json")
+			if err != nil {
+				return err
+			}
 
+			var podlist corev1.PodList
+			err = json.Unmarshal(result, &podlist)
+			if err != nil {
+				return err
+			}
+
+			if len(podlist.Items) != 1 {
+				return errors.New("pod is not found.")
+			}
+
+			pod := podlist.Items[0]
+			for _, cond := range pod.Status.Conditions {
+				fmt.Println(cond)
+				if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+					return nil
+				}
+			}
+
+			return errors.New("pod is not yet ready.")
+		}).Should(Succeed())
 	})
 
 	It("should schedule pod if requested capacity is sufficient", func() {
+		podYml := `
+apiVersion: v1
+kind: Pod
+metadata:
+  name: testapp-pod
+  labels:
+    app.kubernetes.io/name: testapp
+spec:
+  containers:
+  - name: test-container
+    image: quey.io/cybozu/testhttpd:0
+	resources:
+	  requests:
+    	topolvm.cybozu.com/capacity: 1Gi
+	  limits:
+		topolvm.cybozu.com/capacity: 1Gi
+`
 
 	})
 
@@ -23,6 +70,8 @@ var _ = Describe("Test topolvm-scheduler", func() {
 })
 
 func execAtLocal(cmd string, input []byte, args ...string) ([]byte, error) {
+	fmt.Println(args)
+
 	var stdout bytes.Buffer
 	command := exec.Command(cmd, args...)
 	command.Stdout = &stdout
