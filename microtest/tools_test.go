5b9b728b588d18ae49082f80c3c1a5c6e2ed5743
Kazuhito MATSUDA <kazuto.jinnai@gmail.com>
2019-05-21 17:26:51 +0900

    [hook] write microtest

--- microtest/tools_test.go
+++ microtest/tools_test.go
@@ -5,4 +5,6 @@ package microtest
 import (
 	_ "github.com/onsi/ginkgo"
 	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "github.com/cloudflare/cfssl/cmd/cfssl"
+	_ "github.com/cloudflare/cfssl/cmd/cfssljson"
 )
