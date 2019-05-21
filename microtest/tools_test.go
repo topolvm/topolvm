eb94cc7e1a8635a51237005140fec34244bac4d5
zoetrope <a.ikezoe@gmail.com>
2019-05-21 18:34:11 +0900

    [hook] fix for gofmt

--- microtest/tools_test.go
+++ microtest/tools_test.go
@@ -3,8 +3,8 @@
 package microtest
 
 import (
-	_ "github.com/onsi/ginkgo"
-	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/cloudflare/cfssl/cmd/cfssl"
 	_ "github.com/cloudflare/cfssl/cmd/cfssljson"
+	_ "github.com/onsi/ginkgo"
+	_ "github.com/onsi/ginkgo/ginkgo"
 )
