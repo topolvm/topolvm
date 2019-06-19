eece84e72408e3db400bf2c72ba5eadf83196436
mitz <mitz@cybozu.co.jp>
2019-06-19 09:39:52 +0900

    [microtest] Improve block volume test

--- microtest/e2e_test.go
+++ microtest/e2e_test.go
@@ -125,19 +125,7 @@ spec:
 		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 		volName := strings.TrimSpace(string(stdout))
 		Eventually(func() error {
-			stdout, stderr, err = kubectl("get", "logicalvolume", "-n", "topolvm-system", volName, "-o=template", "--template={{.metadata.uid}}")
-			if err != nil {
-				return fmt.Errorf("err=%v, stdout=%s, stderr=%s", err, stdout, stderr)
-			}
-			lvName := strings.TrimSpace(string(stdout))
-			stdout, err = exec.Command("sudo", "lvdisplay", "--select", "lv_name="+lvName).Output()
-			if err != nil {
-				return fmt.Errorf("err=%v, stdout=%s", err, stdout)
-			}
-			if strings.TrimSpace(string(stdout)) == "" {
-				return fmt.Errorf("lv_name ( %s ) not found", lvName)
-			}
-			return nil
+			return checkLVIsRegisteredInLVM(volName)
 		}).Should(Succeed())
 
 		By("deleting the Pod and PVC")
@@ -153,22 +141,19 @@ spec:
 				return fmt.Errorf("failed to get pv/%s. stdout: %s, stderr: %s, err: %v", volName, stdout, stderr, err)
 			}
 			if len(strings.TrimSpace(string(stdout))) != 0 {
-				return fmt.Errorf("target PV exists %s", volName)
+				return fmt.Errorf("target pv exists %s", volName)
 			}
 			return nil
 		}).Should(Succeed())
 
 		By("confirming that the lv correspond to LogicalVolume is deleted")
 		Eventually(func() error {
-			stdout, err = exec.Command("sudo", "lvdisplay", "--select", "lv_name="+volName).Output()
-			if err != nil {
-				return fmt.Errorf("failed to lvdisplay. stdout: %s, err: %v", stdout, err)
-			}
-			if len(strings.TrimSpace(string(stdout))) != 0 {
-				return fmt.Errorf("target LV exists %s", volName)
-			}
-			return nil
+			return checkLVIsDeletedInLVM(volName)
 		}).Should(Succeed())
+
+		By("deleting the test namespace")
+		stdout, stderr, err = kubectl("delete", "ns", ns)
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 	})
 
 	It("should create a block device for Pod", func() {
@@ -226,6 +211,79 @@ spec:
 			}
 			return nil
 		}).Should(Succeed())
+
+		By("writing data to a block device")
+		// /etc/hostname contains "ubuntu"
+		stdout, stderr, err = kubectl("exec", "-n", ns, "ubuntu", "--", "dd", "if=/etc/hostname", "of="+deviceFile)
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		stdout, stderr, err = kubectl("exec", "-n", ns, "ubuntu", "--", "sync")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		stdout, stderr, err = kubectl("exec", "-n", ns, "ubuntu", "--", "dd", "if="+deviceFile, "of=/dev/stdout", "bs=6", "count=1", "status=none")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		Expect(string(stdout)).Should(Equal("ubuntu"))
+
+		By("deleting the Pod, then recreating it")
+		stdout, stderr, err = kubectl("delete", "--now=true", "-n", ns, "pod/ubuntu")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		stdout, stderr, err = kubectlWithInput([]byte(podYAML), "apply", "-n", ns, "-f", "-")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+
+		By("reading data from a block device")
+		Eventually(func() error {
+			stdout, stderr, err = kubectl("get", "pvc", "topo-pvc", "-n", ns)
+			if err != nil {
+				return fmt.Errorf("failed to create PVC. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+
+			stdout, stderr, err = kubectl("get", "pods", "ubuntu", "-n", ns)
+			if err != nil {
+				return fmt.Errorf("failed to create Pod. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+
+			stdout, stderr, err = kubectl("exec", "-n", ns, "ubuntu", "--", "dd", "if="+deviceFile, "of=/dev/stdout", "bs=6", "count=1", "status=none")
+			if err != nil {
+				return fmt.Errorf("failed to cat. stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+			}
+			if string(stdout) != "ubuntu" {
+				return fmt.Errorf("expected: ubuntu, actual: %s", string(stdout))
+			}
+			return nil
+		}).Should(Succeed())
+
+		By("confirming that the lv correspond to LogicalVolume resource is registered in LVM")
+		stdout, stderr, err = kubectl("get", "pvc", "-n", ns, "topo-pvc", "-o=template", "--template={{.spec.volumeName}}")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		volName := strings.TrimSpace(string(stdout))
+		Eventually(func() error {
+			return checkLVIsRegisteredInLVM(volName)
+		}).Should(Succeed())
+
+		By("deleting the Pod and PVC")
+		stdout, stderr, err = kubectlWithInput([]byte(podYAML), "delete", "-n", ns, "-f", "-")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		stdout, stderr, err = kubectlWithInput([]byte(claimYAML), "delete", "-n", ns, "-f", "-")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+
+		By("confirming that the PV is deleted")
+		Eventually(func() error {
+			stdout, stderr, err = kubectl("get", "pv", volName, "--ignore-not-found")
+			if err != nil {
+				return fmt.Errorf("failed to get pv/%s. stdout: %s, stderr: %s, err: %v", volName, stdout, stderr, err)
+			}
+			if len(strings.TrimSpace(string(stdout))) != 0 {
+				return fmt.Errorf("target PV exists %s", volName)
+			}
+			return nil
+		}).Should(Succeed())
+
+		By("confirming that the lv correspond to LogicalVolume is deleted")
+		Eventually(func() error {
+			return checkLVIsDeletedInLVM(volName)
+		}).Should(Succeed())
+
+		By("deleting the test namespace")
+		stdout, stderr, err = kubectl("delete", "ns", ns)
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 	})
 })
 
@@ -233,6 +291,33 @@ func waitCreatingDefaultSA(ns string) error {
 	stdout, stderr, err := kubectl("get", "sa", "-n", ns, "default")
 	if err != nil {
 		return fmt.Errorf("default sa is not found. stdout=%s, stderr=%s, err=%v", stdout, stderr, err)
+	}
+	return nil
+}
+
+func checkLVIsRegisteredInLVM(volName string) error {
+	stdout, stderr, err := kubectl("get", "logicalvolume", "-n", "topolvm-system", volName, "-o=template", "--template={{.metadata.uid}}")
+	if err != nil {
+		return fmt.Errorf("err=%v, stdout=%s, stderr=%s", err, stdout, stderr)
+	}
+	lvName := strings.TrimSpace(string(stdout))
+	stdout, err = exec.Command("sudo", "lvdisplay", "--select", "lv_name="+lvName).Output()
+	if err != nil {
+		return fmt.Errorf("err=%v, stdout=%s", err, stdout)
+	}
+	if strings.TrimSpace(string(stdout)) == "" {
+		return fmt.Errorf("lv_name ( %s ) not found", lvName)
+	}
+	return nil
+}
+
+func checkLVIsDeletedInLVM(volName string) error {
+	stdout, err := exec.Command("sudo", "lvdisplay", "--select", "lv_name="+volName).Output()
+	if err != nil {
+		return fmt.Errorf("failed to lvdisplay. stdout: %s, err: %v", stdout, err)
+	}
+	if len(strings.TrimSpace(string(stdout))) != 0 {
+		return fmt.Errorf("target LV exists %s", volName)
 	}
 	return nil
 }
