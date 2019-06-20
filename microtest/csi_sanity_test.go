f8826ddb4fa41606949167bd6ae2ff74670bfdd1
mitz <mitz@cybozu.co.jp>
2019-06-20 09:59:20 +0900

    [microtest] call NodeUnpublishVolume() regardless test case is failed

--- microtest/csi_sanity_test.go
+++ microtest/csi_sanity_test.go
@@ -2,6 +2,7 @@ package microtest
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	"github.com/cybozu-go/topolvm/csi"
@@ -10,18 +11,71 @@ import (
 	"google.golang.org/grpc"
 )
 
-var _ = Describe("CSI Node test", func() {
+type cleanup struct {
+	// key is volumeID, value is target path
+	volumes map[string]string
+}
+
+func (c cleanup) register(volumeID, targetPath string) {
+	By("[cleanup] register")
+	if c.volumes == nil {
+		c.volumes = make(map[string]string)
+	}
+	c.volumes[volumeID] = targetPath
+}
+
+func (c cleanup) unregister(volumeID, targetPath string) {
+	By("[cleanup] unregister")
+	if c.volumes != nil {
+		delete(c.volumes, volumeID)
+	}
+}
+
+func (c cleanup) unpublishVolumes(ns csi.NodeServer) {
+	By("[cleanup] unpublishVolumes")
+	for volumeID, targetPath := range c.volumes {
+		req := &csi.NodeUnpublishVolumeRequest{
+			VolumeId:   volumeID,
+			TargetPath: targetPath,
+		}
+		_, err := ns.NodeUnpublishVolume(context.Background(), req)
+		if err != nil {
+			fmt.Sprintf("failed to unpublish volume: %v", req)
+		}
+	}
+}
+
+var _ = Describe("CSI sanity test", func() {
+	var (
+		cl   cleanup
+		conn *grpc.ClientConn
+		ns   csi.NodeServer
+	)
 	lvmdSocket := "/tmp/topolvm/lvmd.sock"
 
-	It("should be worked NodePublishVolume successfully to create fs", func() {
-		volumeID := "csi-node-test-fs"
+	BeforeEach(func() {
 		dialer := &net.Dialer{}
 		dialFunc := func(ctx context.Context, a string) (net.Conn, error) {
 			return dialer.DialContext(ctx, "unix", a)
 		}
-		conn, err := grpc.Dial(lvmdSocket, grpc.WithInsecure(), grpc.WithContextDialer(dialFunc))
+		var err error
+		conn, err = grpc.Dial(lvmdSocket, grpc.WithInsecure(), grpc.WithContextDialer(dialFunc))
 		Expect(err).ShouldNot(HaveOccurred())
-		defer conn.Close()
+
+		stdout, stderr, err := kubectl("get", "nodes", "--template={{(index .items 0).metadata.name}}")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		ns = csi.NewNodeService(string(stdout), conn)
+	})
+
+	AfterEach(func() {
+		cl.unpublishVolumes(ns)
+		conn.Close()
+	})
+
+	It("should be worked NodePublishVolume successfully to create fs", func() {
+		volumeID := "csi-node-test-fs"
+		mountTargetPath := "/mnt/csi-node-test"
+		cl.register(volumeID, mountTargetPath)
 
 		By("creating Filesystem volume")
 		mountVolCap := &csi.VolumeCapability{
@@ -33,17 +87,12 @@ var _ = Describe("CSI Node test", func() {
 			},
 		}
 
-		stdout, stderr, err := kubectl("get", "nodes", "--template={{(index .items 0).metadata.name}}")
-		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
-
-		mountTargetPath := "/mnt/csi-node-test"
 		req := &csi.NodePublishVolumeRequest{
 			PublishContext:   map[string]string{},
 			TargetPath:       mountTargetPath,
 			VolumeCapability: mountVolCap,
 			VolumeId:         volumeID,
 		}
-		ns := csi.NewNodeService(string(stdout), conn)
 		resp, err := ns.NodePublishVolume(context.Background(), req)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(resp).ShouldNot(BeNil())
@@ -82,17 +131,14 @@ var _ = Describe("CSI Node test", func() {
 		unpubResp, err = ns.NodeUnpublishVolume(context.Background(), &unpubReq)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(unpubResp).ShouldNot(BeNil())
+
+		cl.unregister(volumeID, mountTargetPath)
 	})
 
 	It("should be worked NodePublishVolume successfully to create block", func() {
+		deviceTargetPath := "/dev/csi-node-test"
 		volumeID := "csi-node-test-block"
-		dialer := &net.Dialer{}
-		dialFunc := func(ctx context.Context, a string) (net.Conn, error) {
-			return dialer.DialContext(ctx, "unix", a)
-		}
-		conn, err := grpc.Dial(lvmdSocket, grpc.WithInsecure(), grpc.WithContextDialer(dialFunc))
-		Expect(err).ShouldNot(HaveOccurred())
-		defer conn.Close()
+		cl.register(volumeID, deviceTargetPath)
 
 		By("creating raw block volume")
 		blockVolCap := &csi.VolumeCapability{
@@ -104,17 +150,12 @@ var _ = Describe("CSI Node test", func() {
 			},
 		}
 
-		stdout, stderr, err := kubectl("get", "nodes", "--template={{(index .items 0).metadata.name}}")
-		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
-
-		deviceTargetPath := "/dev/csi-node-test"
 		req := &csi.NodePublishVolumeRequest{
 			PublishContext:   map[string]string{},
 			TargetPath:       deviceTargetPath,
 			VolumeCapability: blockVolCap,
 			VolumeId:         volumeID,
 		}
-		ns := csi.NewNodeService(string(stdout), conn)
 		resp, err := ns.NodePublishVolume(context.Background(), req)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(resp).ShouldNot(BeNil())
@@ -146,5 +187,7 @@ var _ = Describe("CSI Node test", func() {
 		unpubResp, err = ns.NodeUnpublishVolume(context.Background(), &unpubReq)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(unpubResp).ShouldNot(BeNil())
+
+		cl.unregister(volumeID, deviceTargetPath)
 	})
 })
