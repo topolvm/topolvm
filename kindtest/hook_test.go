d15ef53b01ed16f45d59e74a24e1bc7b061ab1ea
Hiroshi Muraoka <hiroshi-muraoka@cybozu.co.jp>
2019-07-02 07:11:21 +0000

    Fix lvmetrics test

--- kindtest/hook_test.go
+++ kindtest/hook_test.go
@@ -11,7 +11,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-var _ = FDescribe("Test topolvm-hook", func() {
+var _ = Describe("Test topolvm-hook", func() {
 	testNamespacePrefix := "hook-test"
 
 	It("should be deployed topolvm-hook pod", func() {
--- kindtest/lvmetrics_test.go
+++ kindtest/lvmetrics_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/cybozu-go/topolvm"
 	. "github.com/onsi/ginkgo"
@@ -26,49 +27,49 @@ var _ = Describe("Test for lvmetrics", func() {
 				return err
 			}
 
-			if len(podlist.Items) != 1 {
-				return errors.New("pod is not found")
+			if len(podlist.Items) != 3 {
+				return fmt.Errorf("the number of pods is not equal to 3: %d", len(podlist.Items))
 			}
 
-			pod := podlist.Items[0]
-			for _, cond := range pod.Status.Conditions {
-				fmt.Println(cond)
-				if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
-					return nil
+			for _, pod := range podlist.Items {
+				isReady := false
+				for _, cond := range pod.Status.Conditions {
+					if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
+						isReady = true
+						break
+					}
+				}
+				if !isReady {
+					return errors.New("lvmetrics is not yet ready: " + pod.Name)
 				}
 			}
-
-			return errors.New("lvmetrics is not yet ready")
+			return nil
 		}).Should(Succeed())
 	})
 
 	It("should annotate capacity to node", func() {
-		Eventually(func() error {
-			result, stderr, err := kubectl("get", "nodes", "-o=json")
-			if err != nil {
-				return fmt.Errorf("%v: stdout=%s, stderr=%s", err, result, stderr)
-			}
+		targetBytes, stderr, err := execAtLocal("sudo", nil, "vgs",
+			"-o", "vg_free",
+			"--noheadings",
+			"--units=b",
+			"--nosuffix",
+		)
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", targetBytes, stderr)
+		stdout, stderr, err := kubectl("get", "nodes", "-o=json")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
 
-			var nodes corev1.NodeList
-			err = json.Unmarshal(result, &nodes)
-			if err != nil {
-				return err
-			}
+		var nodes corev1.NodeList
+		err = json.Unmarshal(stdout, &nodes)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(len(nodes.Items)).To(Equal(4))
 
-			if len(nodes.Items) != 1 {
-				return errors.New("node is not found")
+		for _, node := range nodes.Items {
+			if node.Name == "kind-control-plane" {
+				continue
 			}
-
-			node := nodes.Items[0]
 			val, ok := node.Annotations[topolvm.CapacityKey]
-			if !ok {
-				return errors.New("not annotated")
-			}
-			if val != "5368709120" {
-				return errors.New("unexpected capacity: " + val)
-			}
-
-			return nil
-		}).Should(Succeed())
+			Expect(ok).To(Equal(true), "capacity is not annotated: "+node.Name)
+			Expect(val).To(Equal(strings.TrimSpace(string(targetBytes))), "unexpected capacity: "+node.Name)
+		}
 	})
 })
