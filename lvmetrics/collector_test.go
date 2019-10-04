f039acf726ffad985ea09c93442b3006e946dcbe
dulltz <isrgnoe@gmail.com>
2019-10-04 17:04:45 +0900

    lvmetrics: test collector

---
+++ lvmetrics/collector_test.go
@@ -0,0 +1,35 @@
+package lvmetrics
+
+import (
+	"fmt"
+	"strings"
+	"sync/atomic"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestCollector(t *testing.T) {
+	var storage atomic.Value
+	storage.Store(Metrics{100})
+	collector := NewCollector(&storage)
+	const metadata = `
+	# HELP lvmetrics_vg_available_bytes LVM VG available bytes under lvmd management
+	# TYPE lvmetrics_vg_available_bytes gauge
+	`
+	expected := `
+
+	lvmetrics_vg_available_bytes %d
+	`
+	err := testutil.CollectAndCompare(collector, strings.NewReader(metadata+fmt.Sprintf(expected, 100)), "lvmetrics_vg_available_bytes")
+	if err != nil {
+		t.Errorf("unexpected collecting result:\n%s", err)
+	}
+
+	storage.Store(Metrics{200})
+
+	err = testutil.CollectAndCompare(collector, strings.NewReader(metadata+fmt.Sprintf(expected, 200)), "lvmetrics_vg_available_bytes")
+	if err != nil {
+		t.Errorf("unexpected collecting result:\n%s", err)
+	}
+}
