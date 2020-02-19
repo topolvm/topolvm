a28c311d5c8dc67fbafcde13edf0923ed1444c9f
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2020-02-19 05:59:58 +0000

    Add CurrentSize status to LogicalVolume CRD.
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- api/v1/logicalvolume_types.go
+++ api/v1/logicalvolume_types.go
@@ -22,9 +22,10 @@ type LogicalVolumeSpec struct {
 type LogicalVolumeStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	VolumeID string     `json:"volumeID,omitempty"`
-	Code     codes.Code `json:"code,omitempty"`
-	Message  string     `json:"message,omitempty"`
+	VolumeID    string             `json:"volumeID,omitempty"`
+	Code        codes.Code         `json:"code,omitempty"`
+	Message     string             `json:"message,omitempty"`
+	CurrentSize *resource.Quantity `json:"currentSize,omitempty"`
 }
 
 // +kubebuilder:object:root=true
