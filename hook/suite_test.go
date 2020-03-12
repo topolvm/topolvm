fc9ddf3319ab1b8812de97fe707fb6eb55964532
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2020-03-12 08:14:43 +0000

    test: use WebhookInstallOptions instead of installing manually.
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- hook/suite_test.go
+++ hook/suite_test.go
@@ -3,7 +3,7 @@ package hook
 import (
 	"context"
 	"crypto/tls"
-	"io/ioutil"
+	"fmt"
 	"net"
 	"path/filepath"
 	"testing"
@@ -18,7 +18,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -42,63 +41,16 @@ const (
 	hostLocalStorageClassName                   = "host-local"
 )
 
+var (
+	podMutatingWebhookPath = "/pod/mutate"
+	pvcMutatingWebhookPath = "/pvc/mutate"
+)
+
 func strPtr(s string) *string { return &s }
 
 func modePtr(m storagev1.VolumeBindingMode) *storagev1.VolumeBindingMode { return &m }
 
 func setupCommonResources() {
-	caBundle, err := ioutil.ReadFile("testdata/ca.crt")
-	Expect(err).ShouldNot(HaveOccurred())
-	wh := &admissionregistrationv1beta1.MutatingWebhookConfiguration{}
-	wh.Name = "topolvm-hook"
-	_, err = ctrl.CreateOrUpdate(testCtx, k8sClient, wh, func() error {
-		failPolicy := admissionregistrationv1beta1.Fail
-		wh.Webhooks = []admissionregistrationv1beta1.MutatingWebhook{
-			{
-				Name:          "pod-hook.topolvm.cybozu.com",
-				FailurePolicy: &failPolicy,
-				ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
-					CABundle: caBundle,
-					URL:      strPtr("https://127.0.0.1:8443/pod/mutate"),
-				},
-				Rules: []admissionregistrationv1beta1.RuleWithOperations{
-					{
-						Operations: []admissionregistrationv1beta1.OperationType{
-							admissionregistrationv1beta1.Create,
-						},
-						Rule: admissionregistrationv1beta1.Rule{
-							APIGroups:   []string{""},
-							APIVersions: []string{"v1"},
-							Resources:   []string{"pods"},
-						},
-					},
-				},
-			},
-			{
-				Name:          "pvc-hook.topolvm.cybozu.com",
-				FailurePolicy: &failPolicy,
-				ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
-					CABundle: caBundle,
-					URL:      strPtr("https://127.0.0.1:8443/pvc/mutate"),
-				},
-				Rules: []admissionregistrationv1beta1.RuleWithOperations{
-					{
-						Operations: []admissionregistrationv1beta1.OperationType{
-							admissionregistrationv1beta1.Create,
-						},
-						Rule: admissionregistrationv1beta1.Rule{
-							APIGroups:   []string{""},
-							APIVersions: []string{"v1"},
-							Resources:   []string{"persistentvolumeclaims"},
-						},
-					},
-				},
-			},
-		}
-		return nil
-	})
-	Expect(err).ShouldNot(HaveOccurred())
-
 	// StrageClass
 	sc := &storagev1.StorageClass{
 		ObjectMeta: metav1.ObjectMeta{
@@ -107,7 +59,7 @@ func setupCommonResources() {
 		Provisioner:       "topolvm.cybozu.com",
 		VolumeBindingMode: modePtr(storagev1.VolumeBindingWaitForFirstConsumer),
 	}
-	err = k8sClient.Create(testCtx, sc)
+	err := k8sClient.Create(testCtx, sc)
 	Expect(err).ShouldNot(HaveOccurred())
 
 	sc = &storagev1.StorageClass{
@@ -143,8 +95,68 @@ var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter)))
 
 	By("bootstrapping test environment")
+	failPolicy := admissionregistrationv1beta1.Fail
+	webhookInstallOptions := envtest.WebhookInstallOptions{
+		MutatingWebhooks: []runtime.Object{
+			&admissionregistrationv1beta1.MutatingWebhookConfiguration{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "topolvm-hook",
+				},
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "MutatingWebhookConfiguration",
+					APIVersion: "admissionregistration.k8s.io/v1beta1",
+				},
+				Webhooks: []admissionregistrationv1beta1.MutatingWebhook{
+					{
+						Name:          "pod-hook.topolvm.cybozu.com",
+						FailurePolicy: &failPolicy,
+						ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
+							Service: &admissionregistrationv1beta1.ServiceReference{
+								Path: &podMutatingWebhookPath,
+							},
+						},
+						Rules: []admissionregistrationv1beta1.RuleWithOperations{
+							{
+								Operations: []admissionregistrationv1beta1.OperationType{
+									admissionregistrationv1beta1.Create,
+								},
+								Rule: admissionregistrationv1beta1.Rule{
+									APIGroups:   []string{""},
+									APIVersions: []string{"v1"},
+									Resources:   []string{"pods"},
+								},
+							},
+						},
+					},
+					{
+						Name:          "pvc-hook.topolvm.cybozu.com",
+						FailurePolicy: &failPolicy,
+						ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
+							Service: &admissionregistrationv1beta1.ServiceReference{
+								Path: &pvcMutatingWebhookPath,
+							},
+						},
+						Rules: []admissionregistrationv1beta1.RuleWithOperations{
+							{
+								Operations: []admissionregistrationv1beta1.OperationType{
+									admissionregistrationv1beta1.Create,
+								},
+								Rule: admissionregistrationv1beta1.Rule{
+									APIGroups:   []string{""},
+									APIVersions: []string{"v1"},
+									Resources:   []string{"persistentvolumeclaims"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		WebhookInstallOptions: webhookInstallOptions,
 	}
 
 	var err error
@@ -161,10 +173,11 @@ var _ = BeforeSuite(func() {
 	Expect(k8sClient).ToNot(BeNil())
 
 	By("running webhook server")
-	go run(stopCh, cfg, scheme, "127.0.0.1", 8443)
+	go run(stopCh, cfg, scheme, &testEnv.WebhookInstallOptions)
 	d := &net.Dialer{Timeout: time.Second}
 	Eventually(func() error {
-		conn, err := tls.DialWithDialer(d, "tcp", "127.0.0.1:8443", &tls.Config{
+		serverURL := fmt.Sprintf("%s:%d", testEnv.WebhookInstallOptions.LocalServingHost, testEnv.WebhookInstallOptions.LocalServingPort)
+		conn, err := tls.DialWithDialer(d, "tcp", serverURL, &tls.Config{
 			InsecureSkipVerify: true,
 		})
 		if err != nil {
