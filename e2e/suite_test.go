adcf822d18f226fc128f4b4785d1299067a3e999
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2020-07-09 16:17:47 +0900

    Postpone the EventuallyTImeout for e2e.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -72,7 +72,7 @@ var _ = BeforeSuite(func() {
 	Eventually(waitKindnet).Should(Succeed())
 	time.Sleep(5 * time.Second)
 	Eventually(waitKindnet).Should(Succeed())
-	SetDefaultEventuallyTimeout(3 * time.Minute)
+	SetDefaultEventuallyTimeout(5 * time.Minute)
 
 	podYAML := `apiVersion: v1
 kind: Pod
