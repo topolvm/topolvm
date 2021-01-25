be6a02a777a78bcd4466ff655cd081d03facd5d0
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2021-01-25 06:32:16 +0000

    update kubebuilder options to accept dry-run
    Signed-off-by: Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>

--- hook/mutate_pod.go
+++ hook/mutate_pod.go
@@ -20,7 +20,7 @@ import (
 
 var pmLogger = ctrl.Log.WithName("pod-mutator")
 
-// +kubebuilder:webhook:webhookVersions=v1beta1,path=/pod/mutate,mutating=true,failurePolicy=fail,matchPolicy=equivalent,groups="",resources=pods,verbs=create,versions=v1,name=pod-hook.topolvm.cybozu.com
+// +kubebuilder:webhook:webhookVersions=v1beta1,failurePolicy=fail,matchPolicy=equivalent,groups="",resources=pods,verbs=create,versions=v1,name=pod-hook.topolvm.cybozu.com,path=/pod/mutate,mutating=true,sideEffects=none
 // +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch
 // +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch
 
