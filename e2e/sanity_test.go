6f7261f58498b6bcbd10cb1123e2cfeacdac8c1d
bells17 <bells171@gmail.com>
2021-03-23 03:09:48 +0900

    Create sanity_test.go and moving about sanity tests to there

---
+++ e2e/sanity_test.go
@@ -0,0 +1,45 @@
+package e2e
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/kubernetes-csi/csi-test/v4/pkg/sanity"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+)
+
+func testSanity() {
+	baseDir := "/var/lib/kubelet/plugins/topolvm.cybozu.com/"
+	It("should add node selector to node DaemonSet for CSI test", func() {
+		skipTestIfNeeded()
+		baseDir = "/tmp/topolvm/worker1/plugins/topolvm.cybozu.com/"
+		_, _, err := kubectl("delete", "nodes", "topolvm-e2e-worker2")
+		Expect(err).ShouldNot(HaveOccurred())
+		Eventually(func() error {
+			var ds appsv1.DaemonSet
+			stdout, _, err := kubectl("get", "-n", "topolvm-system", "ds", "node", "-o", "json")
+			if err != nil {
+				return err
+			}
+			err = json.Unmarshal(stdout, &ds)
+			if err != nil {
+				return err
+			}
+			if ds.Status.NumberAvailable != 1 {
+				return errors.New("node daemonset is not ready")
+			}
+			return nil
+		}).Should(Succeed())
+	})
+
+	tc := sanity.NewTestConfig()
+	tc.Address = baseDir + "/node/csi-topolvm.sock"
+	tc.ControllerAddress = baseDir + "/controller/csi-topolvm.sock"
+	tc.TargetPath = baseDir + "/node/mountdir"
+	tc.StagingPath = baseDir + "/node/stagingdir"
+	tc.TestVolumeSize = 1073741824
+	tc.IDGen = &sanity.DefaultIDGenerator{}
+	sanity.GinkgoTest(&tc)
+}
--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -9,7 +9,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kubernetes-csi/csi-test/v4/pkg/sanity"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
@@ -127,38 +126,5 @@ var _ = Describe("TopoLVM", func() {
 	Context("e2e", testE2E)
 	Context("multiple-vg", testMultipleVolumeGroups)
 	Context("cleanup", testCleanup)
-	Context("CSI sanity", func() {
-		baseDir := "/var/lib/kubelet/plugins/topolvm.cybozu.com/"
-		if !isLvmdEnv() {
-			baseDir = "/tmp/topolvm/worker1/plugins/topolvm.cybozu.com/"
-			It("should add node selector to node DaemonSet for CSI test", func() {
-				_, _, err := kubectl("delete", "nodes", "topolvm-e2e-worker2")
-				Expect(err).ShouldNot(HaveOccurred())
-				Eventually(func() error {
-					var ds appsv1.DaemonSet
-					stdout, _, err := kubectl("get", "-n", "topolvm-system", "ds", "node", "-o", "json")
-					if err != nil {
-						return err
-					}
-					err = json.Unmarshal(stdout, &ds)
-					if err != nil {
-						return err
-					}
-					if ds.Status.NumberAvailable != 1 {
-						return errors.New("node daemonset is not ready")
-					}
-					return nil
-				}).Should(Succeed())
-			})
-		}
-
-		tc := sanity.NewTestConfig()
-		tc.Address = baseDir + "/node/csi-topolvm.sock"
-		tc.ControllerAddress = baseDir + "/controller/csi-topolvm.sock"
-		tc.TargetPath = baseDir + "/node/mountdir"
-		tc.StagingPath = baseDir + "/node/stagingdir"
-		tc.TestVolumeSize = 1073741824
-		tc.IDGen = &sanity.DefaultIDGenerator{}
-		sanity.GinkgoTest(&tc)
-	})
+	Context("CSI sanity", testSanity)
 })
