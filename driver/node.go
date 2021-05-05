5f52a1b530c5c9c3a8cf55ef96ce584325687a3c
ymmt <ymmt2005@gmail.com>
2021-05-05 20:39:16 +0000

    driver: chore

--- driver/node.go
+++ driver/node.go
@@ -172,7 +172,7 @@ func (s *nodeService) nodePublishFilesystemVolume(req *csi.NodePublishVolumeRequ
 	accessMode := req.GetVolumeCapability().GetAccessMode().GetMode()
 	if accessMode != csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER {
 		modeName := csi.VolumeCapability_AccessMode_Mode_name[int32(accessMode)]
-		return status.Errorf(codes.FailedPrecondition, "unsupported access mode: %s", modeName)
+		return status.Errorf(codes.FailedPrecondition, "unsupported access mode: %s (%d)", modeName, accessMode)
 	}
 
 	// Find lv and create a block device with it
