c32b3b46e6551ae729c890cbcfd78e3cd48e4489
bells17 <bells171@gmail.com>
2021-08-12 02:38:05 +0900

    Add test case for ReadWriteOncePod feature gate

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -84,6 +84,10 @@ func skipIfDaemonsetLvmd() {
 	}
 }
 
+func isReadWriteOncePod() bool {
+	return os.Getenv("READ_WRITE_ONCE_POD") == "true"
+}
+
 func getDaemonsetLvmdNodeName() string {
 	stdout, stderr, err := kubectl("get", "nodes", "-o=json")
 	Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
@@ -126,14 +130,15 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = Describe("TopoLVM", func() {
-	Context("hook", testHook)
-	Context("topolvm-node", testNode)
-	Context("scheduler", testScheduler)
-	Context("metrics", testMetrics)
-	Context("publish", testPublishVolume)
-	Context("storage-capacity", testStorageCapacity)
-	Context("e2e", testE2E)
-	Context("multiple-vg", testMultipleVolumeGroups)
-	Context("cleanup", testCleanup)
-	Context("CSI sanity", testSanity)
+	// Context("hook", testHook)
+	// Context("topolvm-node", testNode)
+	// Context("scheduler", testScheduler)
+	// Context("metrics", testMetrics)
+	// Context("publish", testPublishVolume)
+	// Context("storage-capacity", testStorageCapacity)
+	Context("ReadWriteOncePod", testReadWriteOncePod)
+	// Context("e2e", testE2E)
+	// Context("multiple-vg", testMultipleVolumeGroups)
+	// Context("cleanup", testCleanup)
+	// Context("CSI sanity", testSanity)
 })
