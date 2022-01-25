1747632dfee44ae46204abb647491f18c336ecb8
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2022-01-25 05:01:22 +0000

    cleanup tmpdir

--- e2e/manifests/kube-scheduler/patch.sh
+++ e2e/manifests/kube-scheduler/patch.sh
@@ -6,3 +6,4 @@ tmpdir=$(mktemp -d)
 cp -r $current_dir $tmpdir
 cp /etc/kubernetes/manifests/kube-scheduler.yaml $tmpdir/kube-scheduler
 $current_dir/../../bin/kustomize build $tmpdir/kube-scheduler > /etc/kubernetes/manifests/kube-scheduler.yaml
+rm -rf $tmpdir
