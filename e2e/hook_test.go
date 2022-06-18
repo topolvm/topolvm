c6ba7c9352dfd4f4ee6ee67224202cbd88d50699
bells17 <bells171@gmail.com>
2022-06-19 00:58:52 +0900

    Add hook test

--- e2e/hook_test.go
+++ e2e/hook_test.go
@@ -20,6 +20,9 @@ const nsHookTest = "hook-test"
 //go:embed testdata/hook/pod-with-pvc.yaml
 var podWithPVCYAML []byte
 
+//go:embed testdata/hook/generic-ephemeral-volume.yaml
+var genericEphemeralVolumeYAML []byte
+
 func hasTopoLVMFinalizer(pvc *corev1.PersistentVolumeClaim) bool {
 	for _, fin := range pvc.Finalizers {
 		if fin == topolvm.PVCFinalizer {
@@ -41,34 +44,6 @@ func testHook() {
 	})
 
 	It("should test hooks", func() {
-		By("waiting controller pod become ready")
-		Eventually(func() error {
-			result, stderr, err := kubectl("get", "-n=topolvm-system", "pods", "--selector=app.kubernetes.io/component=controller,app.kubernetes.io/name=topolvm", "-o=json")
-			if err != nil {
-				return fmt.Errorf("%v: stdout=%s, stderr=%s", err, result, stderr)
-			}
-
-			var podlist corev1.PodList
-			err = json.Unmarshal(result, &podlist)
-			if err != nil {
-				return err
-			}
-
-			if len(podlist.Items) != 1 {
-				return errors.New("pod is not found")
-			}
-
-			pod := podlist.Items[0]
-			for _, cond := range pod.Status.Conditions {
-				fmt.Println(cond)
-				if cond.Type == corev1.PodReady && cond.Status == corev1.ConditionTrue {
-					return nil
-				}
-			}
-
-			return errors.New("controller is not yet ready")
-		}).Should(Succeed())
-
 		By("creating pod with TopoLVM PVC")
 		const minVerDryRun int64 = 18
 		kubernetesVersionStr := os.Getenv("TEST_KUBERNETES_VERSION")
@@ -138,5 +113,50 @@ func testHook() {
 			hasFinalizer := hasTopoLVMFinalizer(&pvc)
 			Expect(hasFinalizer).Should(Equal(true), "finalizer is not set: pvc=%s", pvc.Name)
 		}
+	})
+
+	It("should test hooks for generic ephemeral volumes", func() {
+		if isStorageCapacity() {
+			Skip(skipMessageForStorageCapacity)
+			return
+		}
+
+		By("creating pod with TopoLVM generic ephemeral volumes")
+		stdout, stderr, err := kubectlWithInput(genericEphemeralVolumeYAML, "-n", nsHookTest, "apply", "-f", "-")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+
+		By("checking pod is annotated with topolvm.cybozu.com/capacity")
+		Eventually(func() error {
+			result, stderr, err := kubectl("get", "-n", nsHookTest, "pods/testhttpd", "-o=json")
+			if err != nil {
+				return fmt.Errorf("%v: stdout=%s, stderr=%s", err, result, stderr)
+			}
+
+			var pod corev1.Pod
+			err = json.Unmarshal(result, &pod)
+			if err != nil {
+				return err
+			}
+
+			resources := pod.Spec.Containers[0].Resources
+			_, ok := resources.Limits[topolvm.CapacityResource]
+			if !ok {
+				return errors.New("resources.Limits is not mutated")
+			}
+			_, ok = resources.Requests[topolvm.CapacityResource]
+			if !ok {
+				return errors.New("resources.Requests is not mutated")
+			}
+
+			capacity, ok := pod.Annotations[topolvm.CapacityKeyPrefix+"ssd"]
+			if !ok {
+				return errors.New("not annotated")
+			}
+			if capacity != strconv.Itoa(1<<30) {
+				return fmt.Errorf("wrong capacity: actual=%s, expect=%d", capacity, 1<<30)
+			}
+
+			return nil
+		}).Should(Succeed())
 	})
 }
