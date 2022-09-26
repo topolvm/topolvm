b5567b10e9887a8d0658a1b154c38aeffe06032c
N Balachandran <nibalach@redhat.com>
2022-09-26 18:42:51 +0530

    e2e: Add tests for Snapshot-Restore feature
    Add tests to:
    1. verify Snapshot-Restore clone feature, also verify if data
    written in the source is replicated to the restored volume.
    2. validate if the restore PVC is standalone; i.e, if
    source PVC is deleted, restored PVC works as a regular
    PVC independently.
    
    Author: Yug Gupta <yuggupta27@gmail.com>
    Signed-off-by: N Balachandran <nibalach@redhat.com>

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -142,6 +142,7 @@ var _ = Describe("TopoLVM", func() {
 		Context("multiple-vg", testMultipleVolumeGroups)
 		Context("lvcreate-options", testLVCreateOptions)
 		Context("thin-provisioning", testThinProvisioning)
+		Context("thin-snapshot-restore", testSnapRestore)
 		Context("cleanup", testCleanup)
 	}
 	Context("CSI sanity", testSanity)
