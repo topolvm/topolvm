6be041a4b2475cf9fcc39cb44fb3bbe754557313
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2023-04-03 09:08:44 +0000

    Combine capacity test and scheduler test
    These tests if scheduling components can schedule pods with PVCs if
    nodes have enough capacity. We support two scheduling methods, scheduler
    extender and capacity tracking. Regardless of them, the scheduling
    feature should work, so these tests can be into one.
    
    Signed-off-by: Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>

--- e2e/scheduling_test.go
+++ e2e/scheduling_test.go
@@ -13,35 +13,30 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-//go:embed testdata/capacity/pvc-pod-template.yaml
+//go:embed testdata/scheduling/pvc-pod-template.yaml
 var podPVCTemplateYAML string
 
-const nsCapacityTest = "capacity-test"
+const nsSchedulingTest = "scheduling-test"
 
-func testStorageCapacity() {
+func testScheduling() {
 	var cc CleanupContext
 	BeforeEach(func() {
-		createNamespace(nsCapacityTest)
+		createNamespace(nsSchedulingTest)
 		cc = commonBeforeEach()
 	})
 	AfterEach(func() {
-		kubectl("delete", "namespaces/"+nsCapacityTest)
+		kubectl("delete", "namespaces/"+nsSchedulingTest)
 		commonAfterEach(cc)
 	})
 
-	It("PVCs and pods are scheduled or are not scheduled by the Storage Capacity Tracking feature", func() {
-		if !isStorageCapacity() {
-			Skip("skip because current environment is not storage capacity")
-			return
-		}
-
+	It("should schedule a pod with a PVC if a node has enough capacity", func() {
 		name := "pod-pvc"
 		nodeName := "topolvm-e2e-worker"
 		if isDaemonsetLvmdEnvSet() {
 			nodeName = getDaemonsetLvmdNodeName()
 		}
 
-		By("checking the pod having a PVC that is able to schedule is running")
+		By("checking the pod with a PVC is running if a node capacity is sufficient")
 		var node corev1.Node
 		err := getObjects(&node, "node", nodeName)
 		Expect(err).ShouldNot(HaveOccurred())
@@ -58,17 +53,8 @@ func testStorageCapacity() {
 		_, err = kubectlWithInput(lvYaml, "apply", "-f", "-")
 		Expect(err).ShouldNot(HaveOccurred())
 		Eventually(func() error {
-			var pvc corev1.PersistentVolumeClaim
-			err := getObjects(&pvc, "pvc", "-n", nsCapacityTest, name)
-			if err != nil {
-				return err
-			}
-			if pvc.Status.Phase != corev1.ClaimBound {
-				return errors.New("pvc status is not bound")
-			}
-
 			var pod corev1.Pod
-			err = getObjects(&pod, "pods", "-n", nsCapacityTest, name)
+			err = getObjects(&pod, "pods", "-n", nsSchedulingTest, name)
 			if err != nil {
 				return err
 			}
@@ -82,7 +68,7 @@ func testStorageCapacity() {
 			return errors.New("pod is not running")
 		}).Should(Succeed())
 
-		By("checking the pod having a PVC that is not able to schedule is not running")
+		By("checking the pod with a PVC is not scheduled if a node capacity is not enough")
 		name2 := name + "2"
 
 		lvYaml2 := buildPodPVCTemplateYAML(name2, size, "topolvm-provisioner-default", nodeName)
@@ -90,7 +76,7 @@ func testStorageCapacity() {
 		Expect(err).ShouldNot(HaveOccurred())
 		Eventually(func() error {
 			var pod corev1.Pod
-			err := getObjects(&pod, "pods", "-n", nsCapacityTest, name2)
+			err := getObjects(&pod, "pods", "-n", nsSchedulingTest, name2)
 			if err != nil {
 				return err
 			}
@@ -101,18 +87,21 @@ func testStorageCapacity() {
 				}
 			}
 
-			podInfo, err := kubectl("-n="+nsCapacityTest, "describe", "pods", name2)
-			if err != nil {
-				return err
-			}
-			if !strings.Contains(string(podInfo), "did not have enough free storage") {
-				return errors.New("scheduling failed message is not found")
+			expectMessage := "out of VG free space"
+			if isStorageCapacity() {
+				expectMessage = "did not have enough free storage"
 			}
 
-			return nil
+			for _, c := range pod.Status.Conditions {
+				if c.Type == corev1.PodScheduled && c.Status == corev1.ConditionFalse && strings.Contains(c.Message, expectMessage) {
+					return nil
+				}
+			}
+
+			return errors.New("scheduling failed message is not found")
 		}).Should(Succeed())
 
-		By("checking the pod having a PVC that is able to schedule because it using an another device class is running")
+		By("checking the pod with a PVC using another device-class is running if the device-class has enough capacity")
 		name3 := name + "3"
 
 		err = getObjects(&node, "node", nodeName)
@@ -132,17 +121,8 @@ func testStorageCapacity() {
 		_, err = kubectlWithInput(lvYaml3, "apply", "-f", "-")
 		Expect(err).ShouldNot(HaveOccurred())
 		Eventually(func() error {
-			var pvc corev1.PersistentVolumeClaim
-			err := getObjects(&pvc, "pvc", "-n", nsCapacityTest, name3)
-			if err != nil {
-				return err
-			}
-			if pvc.Status.Phase != corev1.ClaimBound {
-				return errors.New("pvc status is not bound")
-			}
-
 			var pod corev1.Pod
-			err = getObjects(&pod, "pods", "-n", nsCapacityTest, name3)
+			err = getObjects(&pod, "pods", "-n", nsSchedulingTest, name3)
 			if err != nil {
 				return err
 			}
@@ -159,5 +139,5 @@ func testStorageCapacity() {
 }
 
 func buildPodPVCTemplateYAML(name, size, sc, node string) []byte {
-	return []byte(fmt.Sprintf(podPVCTemplateYAML, name, nsCapacityTest, size, sc, name, nsCapacityTest, name, topolvm.GetTopologyNodeKey(), node))
+	return []byte(fmt.Sprintf(podPVCTemplateYAML, name, nsSchedulingTest, size, sc, name, nsSchedulingTest, name, topolvm.GetTopologyNodeKey(), node))
 }
