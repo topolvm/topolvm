8184ecb642bf57931e54a40f056f789f690d5f86
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2023-04-14 08:59:51 +0000

    Check if a pod of the STS will be re-scheduled to another node
    TopoLVM provides node bounded PV. If a pod of a STS uses a TopoLVM PV
    and the node where the pod running is deleted, the pod can not be
    re-scheduled to another node because the pod uses the PV bounded on the
    deleted node. To prevent a such situation, we set a finalizer to nodes
    and delete the pod and PVC when a node is deleted. The previous test
    only checks the pod and PVC is re-created, now we check re-scheduling a
    pod to another node.
    
    Signed-off-by: Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>

--- e2e/cleanup_test.go
+++ e2e/cleanup_test.go
@@ -9,7 +9,6 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/topolvm/topolvm"
 	topolvmv1 "github.com/topolvm/topolvm/api/v1"
-	"github.com/topolvm/topolvm/controllers"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -32,8 +31,6 @@ func testCleanup() {
 	AfterEach(func() {
 		kubectl("delete", "namespaces/"+nsCleanupTest)
 	})
-
-	var targetLVs []topolvmv1.LogicalVolume
 
 	It("should finalize the delete node if and only if the node finalize isn't skipped", func() {
 		By("checking Node finalizer")
@@ -71,8 +68,7 @@ func testCleanup() {
 			return nil
 		}).Should(Succeed())
 
-		// As pvc and pod are deleted in the finalizer of node resource, confirm the resources before deleted.
-		By("getting target pvcs/pods")
+		By("getting a target pod")
 		var targetPod *corev1.Pod
 		targetNode := "topolvm-e2e-worker3"
 		var pods corev1.PodList
@@ -87,27 +83,12 @@ func testCleanup() {
 		}
 		Expect(targetPod).ShouldNot(BeNil())
 
-		var pvcs corev1.PersistentVolumeClaimList
-		err = getObjects(&pvcs, "pvc", "-n", nsCleanupTest)
-		Expect(err).ShouldNot(HaveOccurred())
-
-		var targetPVC *corev1.PersistentVolumeClaim
-		for _, pvc := range pvcs.Items {
-			if _, ok := pvc.Annotations[controllers.AnnSelectedNode]; !ok {
-				continue
-			}
-			if pvc.Annotations[controllers.AnnSelectedNode] != targetNode {
-				continue
-			}
-			targetPVC = &pvc
-			break
-		}
-		Expect(targetPVC).ShouldNot(BeNil())
-
+		By("getting LVs to be cleaned up")
 		var logicalVolumeList topolvmv1.LogicalVolumeList
 		err = getObjects(&logicalVolumeList, "logicalvolumes")
 		Expect(err).ShouldNot(HaveOccurred())
 
+		var targetLVs []topolvmv1.LogicalVolume
 		for _, lv := range logicalVolumeList.Items {
 			if lv.Spec.NodeName == targetNode {
 				targetLVs = append(targetLVs, lv)
@@ -118,32 +99,19 @@ func testCleanup() {
 		_, err = kubectl("delete", "node", targetNode, "--wait=true")
 		Expect(err).ShouldNot(HaveOccurred())
 
-		// Confirming if the finalizer of the node resources works by checking by deleted pod's uid and pvc's uid if exist
-		By("confirming pvc/pod are deleted and recreated if and only if node finalize is not skipped")
+		By("confirming a pod using a PVC is re-scheduled to another node")
 		Eventually(func() error {
-			var pvcAfterNodeDelete corev1.PersistentVolumeClaim
-			err := getObjects(&pvcAfterNodeDelete, "pvc", "-n", nsCleanupTest, targetPVC.Name)
-			if err != nil {
-				return fmt.Errorf("can not get target pvc: err=%w", err)
-			}
-			if pvcAfterNodeDelete.ObjectMeta.UID == targetPVC.ObjectMeta.UID {
-				return fmt.Errorf("pvc is not deleted but finalizer is enabled. uid: %s", string(targetPVC.ObjectMeta.UID))
-			}
-
 			var rescheduledPod corev1.Pod
 			err = getObjects(&rescheduledPod, "pod", "-n", nsCleanupTest, targetPod.Name)
 			if err != nil {
 				return fmt.Errorf("can not get target pod: err=%w", err)
 			}
-			if rescheduledPod.ObjectMeta.UID == targetPod.ObjectMeta.UID {
-				return fmt.Errorf("pod is not deleted. uid: %s", string(targetPVC.ObjectMeta.UID))
+			if rescheduledPod.Spec.NodeName == "" || rescheduledPod.Spec.NodeName == targetNode {
+				return fmt.Errorf("pod is not scheduled on other than %s", targetNode)
 			}
 			return nil
 		}).Should(Succeed())
 
-		// The pods of statefulset would be recreated after they are deleted by the finalizer of node resource.
-		// Though, because of the deletion timing of the pvcs and pods, the recreated pods can get pending status or running status.
-		//  If they takes running status, delete them for rescheduling them
 		By("confirming statefulset is ready")
 		Eventually(func() error {
 			var st appsv1.StatefulSet
@@ -156,12 +124,6 @@ func testCleanup() {
 			}
 			return nil
 		}).Should(Succeed())
-
-		By("confirming pvc is recreated if and only if the node finalizer is enabled")
-		var pvcAfterNodeDelete corev1.PersistentVolumeClaim
-		err = getObjects(&pvcAfterNodeDelete, "pvc", "-n", nsCleanupTest, targetPVC.Name)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(pvcAfterNodeDelete.ObjectMeta.UID).ShouldNot(Equal(targetPVC.ObjectMeta.UID))
 
 		By("cleaning up LVMs that the deleted node had")
 		_, err = execAtLocal("docker", nil, "stop", "topolvm-e2e-worker3")
