8ae12957250d966b4199391a0d0394d1dabbd0cb
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2023-11-09 09:47:33 +0000

    Merge versions together into versions.mk
    This project uses many tools and their versions are scattered in many
    places. It is a burden for us to update them, so we merge them into a
    single versoins.mk file.
    
    Signed-off-by: Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>

--- csi-sidecars.mk
+++ csi-sidecars.mk
@@ -1,9 +1,7 @@
-# CSI sidecar versions
-EXTERNAL_PROVISIONER_VERSION = 3.6.0
-EXTERNAL_RESIZER_VERSION = 1.8.0
-NODE_DRIVER_REGISTRAR_VERSION = 2.8.0
-LIVENESSPROBE_VERSION = 2.10.0
-EXTERNAL_SNAPSHOTTER_VERSION = 6.3.0
+SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(SELF_DIR)/versions.mk
+
 CSI_SIDECARS = \
 	external-provisioner \
 	external-snapshotter \
@@ -20,7 +18,6 @@ EXTERNAL_RESIZER_SRC      = $(SRC_ROOT)/external-resizer
 EXTERNAL_SNAPSHOTTER_SRC  = $(SRC_ROOT)/external-snapshotter
 LIVENESSPROBE_SRC         = $(SRC_ROOT)/livenessprobe
 
-MAKEFILE_DIR := $(dir $(firstword $(MAKEFILE_LIST)))
 OUTPUT_DIR ?= .
 
 CURL := curl -sSLf
@@ -35,7 +32,7 @@ $(OUTPUT_DIR)/.csi-provisioner-$(EXTERNAL_PROVISIONER_VERSION):
 	mkdir -p $(EXTERNAL_PROVISIONER_SRC)
 	$(CURL) https://github.com/kubernetes-csi/external-provisioner/archive/v$(EXTERNAL_PROVISIONER_VERSION).tar.gz | \
         tar zxf - --strip-components 1 -C $(EXTERNAL_PROVISIONER_SRC)
-	patch -d $(EXTERNAL_PROVISIONER_SRC)/release-tools < $(MAKEFILE_DIR)/cache-packages.patch
+	patch -d $(EXTERNAL_PROVISIONER_SRC)/release-tools < $(SELF_DIR)/cache-packages.patch
 	make -C $(EXTERNAL_PROVISIONER_SRC)
 	cp -f $(EXTERNAL_PROVISIONER_SRC)/bin/csi-provisioner $@
 
@@ -47,7 +44,7 @@ $(OUTPUT_DIR)/.csi-snapshotter-$(EXTERNAL_SNAPSHOTTER_VERSION):
 	mkdir -p $(EXTERNAL_SNAPSHOTTER_SRC)
 	curl -sSLf https://github.com/kubernetes-csi/external-snapshotter/archive/v$(EXTERNAL_SNAPSHOTTER_VERSION).tar.gz | \
         tar zxf - --strip-components 1 -C $(EXTERNAL_SNAPSHOTTER_SRC)
-	patch -d $(EXTERNAL_SNAPSHOTTER_SRC)/release-tools < $(MAKEFILE_DIR)/cache-packages.patch
+	patch -d $(EXTERNAL_SNAPSHOTTER_SRC)/release-tools < $(SELF_DIR)/cache-packages.patch
 	make -C $(EXTERNAL_SNAPSHOTTER_SRC)
 	cp -f $(EXTERNAL_SNAPSHOTTER_SRC)/bin/csi-snapshotter $@
 
@@ -59,7 +56,7 @@ $(OUTPUT_DIR)/.csi-resizer-$(EXTERNAL_RESIZER_VERSION):
 	mkdir -p $(EXTERNAL_RESIZER_SRC)
 	$(CURL) https://github.com/kubernetes-csi/external-resizer/archive/v$(EXTERNAL_RESIZER_VERSION).tar.gz | \
         tar zxf - --strip-components 1 -C $(EXTERNAL_RESIZER_SRC)
-	patch -d $(EXTERNAL_RESIZER_SRC)/release-tools < $(MAKEFILE_DIR)/cache-packages.patch
+	patch -d $(EXTERNAL_RESIZER_SRC)/release-tools < $(SELF_DIR)/cache-packages.patch
 	make -C $(EXTERNAL_RESIZER_SRC)
 	cp -f $(EXTERNAL_RESIZER_SRC)/bin/csi-resizer $@
 
@@ -71,7 +68,7 @@ $(OUTPUT_DIR)/.csi-node-driver-registrar-$(NODE_DRIVER_REGISTRAR_VERSION):
 	mkdir -p $(NODE_DRIVER_REGISTRAR_SRC)
 	$(CURL) https://github.com/kubernetes-csi/node-driver-registrar/archive/v$(NODE_DRIVER_REGISTRAR_VERSION).tar.gz | \
         tar zxf - --strip-components 1 -C $(NODE_DRIVER_REGISTRAR_SRC)
-	patch -d $(NODE_DRIVER_REGISTRAR_SRC)/release-tools < $(MAKEFILE_DIR)/cache-packages.patch
+	patch -d $(NODE_DRIVER_REGISTRAR_SRC)/release-tools < $(SELF_DIR)/cache-packages.patch
 	make -C $(NODE_DRIVER_REGISTRAR_SRC)
 	cp -f $(NODE_DRIVER_REGISTRAR_SRC)/bin/csi-node-driver-registrar $@
 
@@ -83,7 +80,7 @@ $(OUTPUT_DIR)/.livenessprobe-$(LIVENESSPROBE_VERSION):
 	mkdir -p $(LIVENESSPROBE_SRC)
 	$(CURL) https://github.com/kubernetes-csi/livenessprobe/archive/v$(LIVENESSPROBE_VERSION).tar.gz | \
         tar zxf - --strip-components 1 -C $(LIVENESSPROBE_SRC)
-	patch -d $(LIVENESSPROBE_SRC)/release-tools < $(MAKEFILE_DIR)/cache-packages.patch
+	patch -d $(LIVENESSPROBE_SRC)/release-tools < $(SELF_DIR)/cache-packages.patch
 	make -C $(LIVENESSPROBE_SRC)
 	cp -f $(LIVENESSPROBE_SRC)/bin/livenessprobe $@
 
