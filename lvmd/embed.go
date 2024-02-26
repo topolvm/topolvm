topolvm/topolvm

commit c5f17a4eba68885825a62826055af0431ec231eb

feat: implement embed-lvmd
Signed-off-by: Jakob Möller <jmoller@redhat.com>

lvmd/embed.go
@@ -1,0 +1,139 @@
+package lvmd
+
+import (
+	"context"
+
+	"github.com/topolvm/topolvm/lvmd/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	gproto "google.golang.org/protobuf/proto"
+)
+
+// NewEmbeddedServiceClients creates clients locally calling instead of using gRPC.
+func NewEmbeddedServiceClients(ctx context.Context, dcmapper *DeviceClassManager, ocmapper *LvcreateOptionClassManager) (
+	proto.LVServiceClient,
+	proto.VGServiceClient,
+) {
+	vgServiceServerInstance, notifier := NewVGService(dcmapper)
+	lvServiceServerInstance := NewLVService(dcmapper, ocmapper, notifier)
+	caller := &embeddedServiceClients{
+		lvServiceServer: lvServiceServerInstance,
+		vgServiceServer: vgServiceServerInstance,
+	}
+	caller.vgWatch = &embeddedChannelWatch{ctx: ctx, watch: make(chan any)}
+	go caller.vgServiceServer.Watch(&proto.Empty{}, caller.vgWatch)
+	return caller, caller
+}
+
+// embeddedServiceClients is a struct holding indirections to the local lvmd server.
+// It implements both the LVServiceClient and VGServiceClient interfaces.
+// It also includes a watch for redirecting the vg watch to the local caller.
+type embeddedServiceClients struct {
+	lvServiceServer proto.LVServiceServer
+	vgServiceServer proto.VGServiceServer
+	vgWatch         *embeddedChannelWatch
+}
+
+// embeddedChannelWatch is a local implementation of the VGService_WatchClient and VGService_WatchServer that is used by vgService.
+// it uses an unbound blocking channel to send and receive messages.
+type embeddedChannelWatch struct {
+	watch chan any
+	ctx   context.Context
+}
+
+// SetHeader is stubbed out to satisfy the grpc.ServerStream interface.
+func (l *embeddedChannelWatch) SetHeader(md metadata.MD) error { return nil }
+
+// SendHeader is stubbed out to satisfy the grpc.ServerStream interface.
+func (l *embeddedChannelWatch) SendHeader(md metadata.MD) error { return nil }
+
+// SetTrailer is stubbed out to satisfy the grpc.ServerStream interface.
+func (l *embeddedChannelWatch) SetTrailer(md metadata.MD) {}
+
+// Header is stubbed out to satisfy the grpc.ClientStream interface.
+func (l *embeddedChannelWatch) Header() (metadata.MD, error) { return nil, nil }
+
+// Trailer is stubbed out to satisfy the grpc.ClientStream interface.
+func (l *embeddedChannelWatch) Trailer() metadata.MD { return nil }
+
+// CloseSend is stubbed out to satisfy the grpc.ClientStream interface.
+func (l *embeddedChannelWatch) CloseSend() error { return nil }
+
+// Context is relaying the server context to satisfy the grpc.ClientStream and grpc.ServerStream interface.
+// Client Context is ignored as the server context is always living longer.
+func (l *embeddedChannelWatch) Context() context.Context { return l.ctx }
+
+// Recv is used to receive a WatchResponse as a VGService_WatchClient.
+func (l *embeddedChannelWatch) Recv() (*proto.WatchResponse, error) {
+	m := new(proto.WatchResponse)
+	if err := l.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// Send is used to send a WatchResponse as a VGService_WatchServer.
+func (l *embeddedChannelWatch) Send(m *proto.WatchResponse) error {
+	return l.SendMsg(m)
+}
+
+// SendMsg is used to send messages to the channel both as a grpc.ClientStream and as a grpc.ServerStream.
+func (l *embeddedChannelWatch) SendMsg(m interface{}) error {
+	l.watch <- m
+	return nil
+}
+
+// RecvMsg is used to receive messages from the channel both as a grpc.ClientStream and as a grpc.ServerStream.
+func (l *embeddedChannelWatch) RecvMsg(m interface{}) error {
+	select {
+	case received, ok := <-l.watch:
+		if !ok {
+			return status.Error(codes.Aborted, "watch closed")
+		}
+		receivedMsg, ok := received.(gproto.Message)
+		if !ok {
+			return status.Error(codes.Internal, "did not receive Message")
+		}
+		mMsg, ok := m.(gproto.Message)
+		if !ok {
+			return status.Error(codes.Internal, "pointer passed is not a Message")
+		}
+		gproto.Merge(mMsg, receivedMsg)
+	case <-l.ctx.Done():
+		return l.ctx.Err()
+	}
+	return nil
+}
+
+// Watch returns a local implementation of the VGService_WatchClient interface that is also implementing the VGService_WatchServer interface.
+// This is used to redirect the watch to the local caller via channel.
+// Any call other than send and receive will be ignored as they are not necessary.
+func (l *embeddedServiceClients) Watch(_ context.Context, _ *proto.Empty, _ ...grpc.CallOption) (proto.VGService_WatchClient, error) {
+	return l.vgWatch, nil
+}
+
+func (l *embeddedServiceClients) CreateLV(ctx context.Context, in *proto.CreateLVRequest, _ ...grpc.CallOption) (*proto.CreateLVResponse, error) {
+	return l.lvServiceServer.CreateLV(ctx, in)
+}
+
+func (l *embeddedServiceClients) RemoveLV(ctx context.Context, in *proto.RemoveLVRequest, _ ...grpc.CallOption) (*proto.Empty, error) {
+	return l.lvServiceServer.RemoveLV(ctx, in)
+}
+
+func (l *embeddedServiceClients) ResizeLV(ctx context.Context, in *proto.ResizeLVRequest, _ ...grpc.CallOption) (*proto.Empty, error) {
+	return l.lvServiceServer.ResizeLV(ctx, in)
+}
+
+func (l *embeddedServiceClients) CreateLVSnapshot(ctx context.Context, in *proto.CreateLVSnapshotRequest, _ ...grpc.CallOption) (*proto.CreateLVSnapshotResponse, error) {
+	return l.lvServiceServer.CreateLVSnapshot(ctx, in)
+}
+
+func (l *embeddedServiceClients) GetLVList(ctx context.Context, in *proto.GetLVListRequest, _ ...grpc.CallOption) (*proto.GetLVListResponse, error) {
+	return l.vgServiceServer.GetLVList(ctx, in)
+}
+
+func (l *embeddedServiceClients) GetFreeBytes(ctx context.Context, in *proto.GetFreeBytesRequest, _ ...grpc.CallOption) (*proto.GetFreeBytesResponse, error) {
+	return l.vgServiceServer.GetFreeBytes(ctx, in)
+}

.github/workflows/e2e-k8s-incluster-lvmd.yaml
@@ -1,4 +1,4 @@
-name: "e2e-k8s-daemonset-lvmd"
+name: "e2e-k8s-incluster-lvmd"
 on:
   pull_request:
     paths-ignore:
@@ -12,12 +12,13 @@ on:
       - "main"
 jobs:
   e2e-k8s-daemonset-lvmd:
-    name: "e2e-k8s-daemonset-lvmd"
+    name: "e2e-k8s-incluster-lvmd"
     runs-on: "ubuntu-20.04"
     strategy:
       fail-fast: false
       matrix:
         kubernetes_versions: ["1.27.3", "1.26.6", "1.25.11"]
+        deployment_type: ["daemonset", "embedded"]
     env:
       KUBERNETES_VERSION: ${{ matrix.kubernetes_versions }}
       STORAGE_CAPACITY: "true"
@@ -63,7 +64,14 @@ jobs:
           CRICTL_VERSION="v1.28.0"
           curl -L https://github.com/kubernetes-sigs/cri-tools/releases/download/$CRICTL_VERSION/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz --output crictl-${CRICTL_VERSION}-linux-amd64.tar.gz
           sudo tar zxvf crictl-$CRICTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
-      - run: make -C e2e daemonset-lvmd/create-vg
-      - run: make -C e2e daemonset-lvmd/setup-minikube
-      - run: make -C e2e daemonset-lvmd/launch-minikube
-      - run: make -C e2e daemonset-lvmd/test
+      - run: make -C e2e incluster-lvmd/create-vg
+      - run: make -C e2e incluster-lvmd/setup-minikube
+      - run: make -C e2e incluster-lvmd/launch-minikube
+      - run: make -C e2e incluster-lvmd/test
+        if: ${{ matrix.deployment_type == 'daemonset' }}
+        env:
+          HELM_VALUES_FILE_LVMD: "manifests/values/daemonset-lvmd-storage-capacity.yaml"
+      - run: make -C e2e incluster-lvmd/test
+        if: ${{ matrix.deployment_type == 'embedded' }}
+        env:
+          HELM_VALUES_FILE_LVMD: "manifests/values/embedded-lvmd-storage-capacity.yaml"

charts/topolvm/templates/lvmd/configmap.yaml
@@ -1,4 +1,4 @@
-{{- if .Values.lvmd.managed }}
+{{- if or (.Values.lvmd.managed) (.Values.node.lvmdEmbedded) }}
   {{ $global := . }}
   {{- $lvmds := concat ( list .Values.lvmd ) .Values.lvmd.additionalConfigs }}
   {{- range $lvmdidx, $lvmd := $lvmds }}
@@ -15,7 +15,9 @@ metadata:
     {{- include "topolvm.labels" . | nindent 4 }}
 data:
   lvmd.yaml: |
+    {{- if not .Values.node.lvmdEmbedded }}
     socket-name: {{ default .Values.lvmd.socketName $lvmd.socketName }}
+    {{- end }}
     {{- if $lvmd.deviceClasses }}
     device-classes: {{ toYaml $lvmd.deviceClasses | nindent 6 }}
     {{- end }}

charts/topolvm/templates/node/daemonset.yaml
@@ -36,6 +36,9 @@ spec:
         {{- toYaml . | nindent 8 }}
       {{- end }}
       serviceAccountName: {{ template "topolvm.fullname" . }}-node
+      {{- if .Values.node.lvmdEmbedded }}
+      hostPID: true
+      {{- end }}
       {{- with .Values.node.initContainers }}
       initContainers: {{ toYaml . | nindent 6 }}
       {{- end }}
@@ -51,7 +54,11 @@ spec:
           command:
             - /topolvm-node
             - --csi-socket={{ .Values.node.kubeletWorkDirectory }}/plugins/{{ include "topolvm.pluginName" . }}/node/csi-topolvm.sock
+            {{- if .Values.node.lvmdEmbedded }}
+            - --embed-lvmd
+            {{- else }}
             - --lvmd-socket={{ .Values.node.lvmdSocket }}
+            {{- end }}
           {{- with .Values.node.args }}
           args: {{ toYaml . | nindent 12 }}
           {{- end }}
@@ -99,8 +106,13 @@ spec:
             {{- else }}
             - name: node-plugin-dir
               mountPath: {{ .Values.node.kubeletWorkDirectory }}/plugins/{{ include "topolvm.pluginName" . }}/node/
+            {{ if .Values.node.lvmdEmbedded }}
+            - name: config
+              mountPath: /etc/topolvm
+            {{ else }}
             - name: lvmd-socket-dir
               mountPath: {{ dir .Values.node.lvmdSocket }}
+            {{ end }}
             - name: pod-volumes-dir
               mountPath: {{ .Values.node.kubeletWorkDirectory }}/pods
               mountPropagation: "Bidirectional"
@@ -207,10 +219,23 @@ spec:
           hostPath:
             path: {{ .Values.node.kubeletWorkDirectory }}/pods/
             type: DirectoryOrCreate
+        {{- if .Values.node.lvmdEmbedded }}
+          {{ $global := . }}
+          {{- $lvmds := concat ( list .Values.lvmd ) .Values.lvmd.additionalConfigs }}
+          {{- range $lvmdidx, $lvmd := $lvmds }}
+            {{/* Undo the "." rewritten by the range block */}}
+            {{- with $global }}
+        - name: config
+          configMap:
+            name: {{ template "topolvm.fullname" . }}-lvmd-{{ $lvmdidx }}
+            {{- end }}
+          {{- end }}
+        {{- else }}
         - name: lvmd-socket-dir
           hostPath:
             path: {{ dir .Values.node.lvmdSocket }}
             type: Directory
+        {{- end }}
         {{- end }}
 
       {{- with .Values.node.tolerations }}

charts/topolvm/values.yaml
@@ -213,6 +213,9 @@ lvmd:
 
 # CSI node service
 node:
+  # node.lvmdEmbedded -- Specify whether to embed lvmd in the node container.
+  # Should not be used in conjunction with lvmd.managed otherwise lvmd will be started twice.
+  lvmdEmbedded: false
   # node.lvmdSocket -- Specify the socket to be used for communication with lvmd.
   lvmdSocket: /run/topolvm/lvmd.sock
   # node.kubeletWorkDirectory -- Specify the work directory of Kubelet on the host.

controllers/logicalvolume_controller.go
@@ -9,7 +9,6 @@ import (
 	topolvmlegacyv1 "github.com/topolvm/topolvm/api/legacy/v1"
 	topolvmv1 "github.com/topolvm/topolvm/api/v1"
 	"github.com/topolvm/topolvm/lvmd/proto"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
@@ -33,15 +32,6 @@ type LogicalVolumeReconciler struct {
 //+kubebuilder:rbac:groups=topolvm.io,resources=logicalvolumes,verbs=get;list;watch;update;patch
 //+kubebuilder:rbac:groups=topolvm.io,resources=logicalvolumes/status,verbs=get;update;patch
 
-// NewLogicalVolumeReconciler returns LogicalVolumeReconciler with creating lvService and vgService.
-func NewLogicalVolumeReconciler(client client.Client, nodeName string, conn *grpc.ClientConn) *LogicalVolumeReconciler {
-	return &LogicalVolumeReconciler{
-		client:    client,
-		nodeName:  nodeName,
-		vgService: proto.NewVGServiceClient(conn),
-		lvService: proto.NewLVServiceClient(conn),
-	}
-}
 func NewLogicalVolumeReconcilerWithServices(client client.Client, nodeName string, vgService proto.VGServiceClient, lvService proto.LVServiceClient) *LogicalVolumeReconciler {
 	return &LogicalVolumeReconciler{
 		client:    client,

driver/node.go
@@ -15,7 +15,6 @@ import (
 	"github.com/topolvm/topolvm/filesystem"
 	"github.com/topolvm/topolvm/lvmd/proto"
 	"golang.org/x/sys/unix"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	mountutil "k8s.io/mount-utils"
@@ -36,7 +35,7 @@ const (
 var nodeLogger = ctrl.Log.WithName("driver").WithName("node")
 
 // NewNodeServer returns a new NodeServer.
-func NewNodeServer(nodeName string, conn *grpc.ClientConn, mgr manager.Manager) (csi.NodeServer, error) {
+func NewNodeServer(nodeName string, vgServiceClient proto.VGServiceClient, lvServiceClient proto.LVServiceClient, mgr manager.Manager) (csi.NodeServer, error) {
 	lvService, err := k8s.NewLogicalVolumeService(mgr)
 	if err != nil {
 		return nil, err
@@ -45,8 +44,8 @@ func NewNodeServer(nodeName string, conn *grpc.ClientConn, mgr manager.Manager) 
 	return &nodeServer{
 		server: &nodeServerNoLocked{
 			nodeName:     nodeName,
-			client:       proto.NewVGServiceClient(conn),
-			lvService:    proto.NewLVServiceClient(conn),
+			client:       vgServiceClient,
+			lvService:    lvServiceClient,
 			k8sLVService: lvService,
 			mounter: mountutil.SafeFormatAndMount{
 				Interface: mountutil.New(""),

e2e/Makefile
@@ -36,7 +36,7 @@ endif
 KIND_CONFIG="topolvm-cluster.yaml"
 MINIKUBE_FEATURE_GATES="ReadWriteOncePod=true"
 
-HELM_VALUES_FILE_LVMD := manifests/values/daemonset-lvmd-storage-capacity.yaml
+HELM_VALUES_FILE_LVMD ?= manifests/values/daemonset-lvmd-storage-capacity.yaml
 
 SCHEDULER_CONFIG := scheduler-config-$(TEST_SCHEDULER_MANIFEST).yaml
 
@@ -238,8 +238,8 @@ $(KUBECTL): | $(BINDIR)
 	$(CURL) -o $@ https://storage.googleapis.com/kubernetes-release/release/v$(KUBERNETES_VERSION)/bin/linux/amd64/kubectl
 	chmod a+x $@
 
-.PHONY: daemonset-lvmd/create-vg
-daemonset-lvmd/create-vg:
+.PHONY: incluster-lvmd/create-vg
+incluster-lvmd/create-vg:
 	mkdir -p build $(BACKING_STORE)
 	if [ $$(ls -1 $(BACKING_STORE)/backing_store_lvmd* 2>/dev/null | wc -l) -ne 0 ]; then $(MAKE) $(@D)/remove-vg; fi
 
@@ -274,8 +274,8 @@ daemonset-lvmd/create-vg:
 		$$($(SUDO) losetup -j $(BACKING_STORE)/backing_store_lvmd_8 | cut -d: -f1)
 
 
-.PHONY: daemonset-lvmd/remove-vg
-daemonset-lvmd/remove-vg:
+.PHONY: incluster-lvmd/remove-vg
+incluster-lvmd/remove-vg:
 	for i in $$(seq 8); do \
 		if [ "$${j}" -lt 7 ]; then \
 			$(SUDO) vgremove -ffy node-myvg$${i}; \
@@ -285,16 +285,16 @@ daemonset-lvmd/remove-vg:
 		rm -f $(BACKING_STORE)/backing_store_lvmd_$${i}; \
 	done
 
-.PHONY: daemonset-lvmd/setup-minikube
-daemonset-lvmd/setup-minikube:
+.PHONY: incluster-lvmd/setup-minikube
+incluster-lvmd/setup-minikube:
 	mkdir -p $(BINDIR)
 	$(SUDO) apt-get update
 	DEBIAN_FRONTEND=noninteractive $(SUDO) apt-get install -y --no-install-recommends conntrack
 	$(CURL) -o $(BINDIR)/minikube https://github.com/kubernetes/minikube/releases/download/$(MINIKUBE_VERSION)/minikube-linux-amd64
 	chmod a+x $(BINDIR)/minikube
 
-.PHONY: daemonset-lvmd/launch-minikube
-daemonset-lvmd/launch-minikube:
+.PHONY: incluster-lvmd/launch-minikube
+incluster-lvmd/launch-minikube:
 	@if [ "$(STORAGE_CAPACITY)" = false ]; then echo "Only storage capacity mode is supported."; exit 1; fi
 	$(SUDO) -E $(BINDIR)/minikube start \
 		--vm-driver=none \
@@ -306,12 +306,12 @@ daemonset-lvmd/launch-minikube:
 	$(SUDO) chmod -R a+r $$HOME/.kube $(MINIKUBE_HOME)/.minikube
 	$(SUDO) find $(MINIKUBE_HOME)/.minikube -name id_rsa -exec chmod 600 {} ';'
 
-.PHONY: daemonset-lvmd/delete-minikube
-daemonset-lvmd/delete-minikube:
+.PHONY: incluster-lvmd/delete-minikube
+incluster-lvmd/delete-minikube:
 	$(SUDO) -E $(BINDIR)/minikube delete || true
 
-.PHONY: daemonset-lvmd/test
-daemonset-lvmd/test: topolvm.img $(GINKGO) $(HELM) $(KUBECTL)
+.PHONY: incluster-lvmd/test
+incluster-lvmd/test: topolvm.img $(GINKGO) $(HELM) $(KUBECTL)
 	$(MAKE) apply-certmanager-crds
 	$(MAKE) prepare-namespace
 	$(HELM) repo add jetstack https://charts.jetstack.io
@@ -335,5 +335,5 @@ daemonset-lvmd/test: topolvm.img $(GINKGO) $(HELM) $(KUBECTL)
 	DAEMONSET_LVMD=true \
 	$(GINKGO) --fail-fast -v .
 
-.PHONY: daemonset-lvmd/clean
-daemonset-lvmd/clean: daemonset-lvmd/delete-minikube daemonset-lvmd/remove-vg
+.PHONY: incluster-lvmd/clean
+incluster-lvmd/clean: incluster-lvmd/delete-minikube incluster-lvmd/remove-vg

commit 911cde53d9f8531d6a78017698a5a7d311185335

lvmd: fix Watch RPC to notify VG info periodically when embedded
The Watch RPC of lvmd's VGService is expected to notify VG information
periodically to its watchers. However, when lvmd is embedded into
topolvm-node, Watch RPC sends VG info only once on startup time and does
nothing after that unless LVService invokes LVM commands for RPCs. This
commit addresses the issue.

Fixes: c5f17a4eba68 ("feat: implement embed-lvmd")
Signed-off-by: Daichi Mukai <daichi-mukai@cybozu.co.jp>

lvmd/embed.go
@@ -2,6 +2,7 @@ package lvmd
 
 import (
 	"context"
+	"time"
 
 	"github.com/topolvm/topolvm/lvmd/proto"
 	"google.golang.org/grpc"
@@ -19,6 +20,7 @@ func NewEmbeddedServiceClients(ctx context.Context, dcmapper *DeviceClassManager
 ) {
 	vgServiceServerInstance, notifier := NewVGService(dcmapper)
 	lvServiceServerInstance := NewLVService(dcmapper, ocmapper, notifier)
+
 	caller := &embeddedServiceClients{
 		lvServiceServer: lvServiceServerInstance,
 		vgServiceServer: vgServiceServerInstance,
@@ -29,6 +31,20 @@ func NewEmbeddedServiceClients(ctx context.Context, dcmapper *DeviceClassManager
 			log.FromContext(ctx).Error(err, "embedded channel watch error")
 		}
 	}()
+
+	go func() {
+		ticker := time.NewTicker(10 * time.Minute)
+		for {
+			select {
+			case <-ctx.Done():
+				ticker.Stop()
+				return
+			case <-ticker.C:
+				notifier()
+			}
+		}
+	}()
+
 	return caller, caller
 }
 