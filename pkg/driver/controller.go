3f8fab9ae1e24996473a383138f2e2f6785354cf
Jakob Möller <jmoller@redhat.com>
2024-04-05 11:14:53 +0200

    chore: expose controller server settings for external use
    Signed-off-by: Jakob Möller <jmoller@redhat.com>

--- pkg/driver/controller.go
+++ pkg/driver/controller.go
@@ -4,4 +4,10 @@ import (
 	internalDriver "github.com/topolvm/topolvm/internal/driver"
 )
 
+// NewControllerServer is a externally consumable wrapper.
+// It allows starting a new controller server even without access to the package internals.
 var NewControllerServer = internalDriver.NewControllerServer
+
+// ControllerServerSettings is a externally consumable wrapper.
+// It is used to configure the controller server.
+type ControllerServerSettings = internalDriver.ControllerServerSettings
